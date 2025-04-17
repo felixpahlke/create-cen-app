de6bbfed0fc46d8f5f97de607ea1ce802706c917
felixpahlke <felix.pahlke@ibm.com>
2025-04-17 12:46:26 +0200

    refactor: More cleanup

--- cli/src/utils/checkForPoetry.ts
+++
@@ -1,8 +0,0 @@
-import { execa } from "execa";
-
-export const checkForPoetry = async () => {
-  const poetryInstalled = await execa("poetry", ["--version"])
-    .then(() => true)
-    .catch(() => false);
-  return poetryInstalled;
-};
